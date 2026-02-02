a96eb14e2abce7ec302397a954ba6b4f0f9c8ef4
Kushagra3355 <kushagra.omar@gmail.com>
2026-02-02 10:59:22 +0530

    frontend for DEMO

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def transform_text(text):
     return " ".join(y)
 
 
-tfidf = pickle.load(open("vectorizer.pkl", "rb"))
-model = pickle.load(open("model.pkl", "rb"))
+tfidf = pickle.load(open("models/vectorizer.pkl", "rb"))
+model = pickle.load(open("models/model.pkl", "rb"))
 
 st.title("Email/SMS Spam Classifier")
 
